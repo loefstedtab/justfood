8100dce794580d84656fd251f3fccfc7d1d71f63
Salvador <frrslo@yahoo.com>
2023-01-27 12:07:46 -0500

    wrote all models for DB

---
+++ server/db/models/cooked.js
@@ -0,0 +1,29 @@
+const sequelize = require("sequelize");
+const { model } = require("../db");
+const db = require("../db")
+
+const isCooked = db.define("isCooked", {
+    id(findByPk) {
+        type: sequelize.INTEGER,
+        primaryKey; true,
+        autoIncrement; true,
+        allowNull; false
+
+    },
+user_id: {
+    type: sequelize.INTEGER,
+    allowNull: false
+},
+recipe_id: {
+    type: sequelize.INTEGER,
+    allowNull: false
+},
+isCooked: {
+    type: sequelize.BOOLEAN,
+    allowNull: false,
+},
+
+})
+
+module.exports = isCooked
+
---
+++ server/db/models/pantry.js
@@ -0,0 +1,28 @@
+const sequelize = require("sequelize");
+const db = require("../db")
+
+const Pantry = db.define("pantry", {
+    Pantry_id(findByPk) {
+        type: sequelize.INTEGER,
+        primaryKey; true,
+        autoIncrement; true,
+        allowNull; false
+    },
+user_id: {
+    type: sequelize.INTEGER,
+    allowNull: false
+},
+ingredients: {
+    type: sequelize.STRING,
+    allowNull: false
+},
+quantity: {
+    type: sequelize.INTEGER,
+    allowNull: false
+},
+
+})
+
+
+module.exports = Pantry
+
