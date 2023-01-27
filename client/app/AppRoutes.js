d97457ae6857fddf701d336f51f1300df1356e4f
Armand Arslanian <aarslanian56@gmail.com>
2023-01-27 15:02:52 -0500

    created MyAccount / AboutUs / Pantry components

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -4,6 +4,9 @@ import { Route, Routes } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 import Home from '../components/Home';
 import { me } from './store';
+import AboutUs from '../components/AboutUs';
+import MyAccount from '../components/MyAccount';
+import Pantry from '../components/Pantry';
 
 const AppRoutes = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
@@ -21,6 +24,9 @@ const AppRoutes = () => {
           <Route path="/*" element={<Home />} />
           <Route to="/home" element={<Home />} />
           <Route path="/login" element={<AuthForm name="login" displayName="Login" />}/>
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/myaccount" element={<MyAccount />} />
+          <Route path="/pantry" element={<Pantry />} />
         </Routes>
     </div>
   );
