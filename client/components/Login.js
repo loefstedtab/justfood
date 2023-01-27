0dbe5dad465c13e81e4d97390eb269842ee1b6f6
Lionel Verrier <lgv3lee@gmail.com>
2023-01-27 08:00:34 -0500

    got rid of some commented code, adjusted package.json to start project with npm start insted of npm run start:dev

---
+++ client/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GoogleButton from 'react-google-button';
+
+const Login = () => {
+  return (
+    <div className='Login'>
+      <GoogleButton />
+      
+    </div>
+  );
+};
+
+export default Login
