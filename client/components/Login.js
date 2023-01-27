bc6e90bd51bc3efaf93efe69c7592acad00b29a9
Lionel Verrier <lgv3lee@gmail.com>
2023-01-27 09:20:33 -0500

    started google button functionality, need to redirect user at the router callback

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import GoogleButton from 'react-google-button';
+import { Link }from 'react-router-dom';
+
 
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const redirectToGoogle = `${process.env.CLIENT_URL}/auth/google`;
+    window.open(redirectToGoogle, '_self');
+  };
+
   return (
     <div className='Login'>
-      <GoogleButton />
-      
+      <GoogleButton onClick={handleLogin} />
     </div>
   );
 };
