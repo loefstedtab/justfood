8217376e2133ae1f7f9b4b89be7a38e0a08a7910
Armand Arslanian <aarslanian56@gmail.com>
2023-02-03 17:05:33 -0500

    Navbar.js styling, minus colors, done

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -7,26 +7,35 @@ const Navbar = () => {
   const jwtUser = useSelector((state) => state.auth.getMe)
   return (
     <div>
-      <Link to="/home"><h1 className="WebsiteTitle">Just Food!</h1> </Link>
         {googleUser.loggedIn || jwtUser ? (
           <div>
-            <Link to="/aboutus">About Us</Link>
-            <Link to="/pantry">Pantry</Link>
-            <Link to="/cookinghistory">Cooking History</Link>
-            <Link to="/bookmarks">Bookmarks</Link>
-            <Link to="/myaccount">My Account</Link>
-            <h3>Welcome, {googleUser.firstName || jwtUser.firstName}!</h3>
-            <form action="/auth/logout" method="post">
-              <button className="logout" type="submit">
-                Sign out
-              </button>
-            </form>
+            <Link to="/home"><h1 className="WebsiteTitle">Just Food!</h1> </Link>
+            <div className='NavbarLinks'>
+              <Link to="/pantry">Pantry</Link>
+              <Link to="/myaccount">My Account</Link>
+              <Link to="/cookinghistory">Cooking History</Link>
+              <Link to="/bookmarks">Bookmarks</Link>
+              <Link to="/aboutus">About Us</Link>
+            </div>
+
+            <div>
+              <h3 className="WelcomeElement">Welcome, {googleUser.firstName || jwtUser.firstName}!</h3>
+              <form className='logoutButton' action="/auth/logout" method="post">
+                <button className="logout" type="submit">
+                  Sign out
+                </button>
+              </form>
+            </div>
+
           </div>
         ) : (
           <div>
-            <h3>Welcome, Guest!</h3>
-            <Link to="/login">Login/Sign-up</Link>
-            <Link to="/aboutus">About Us</Link>
+            <Link to="/home"><h1 className="WebsiteTitle">Just Food!</h1> </Link>
+            <h3 className="WelcomeElement">Welcome, Guest!</h3>
+            <div className='NavbarLinks'>
+              <Link to="/login">Login/Sign-up</Link>
+              <Link to="/aboutus">About Us</Link>
+            </div>
           </div>
         )}
       <hr />
