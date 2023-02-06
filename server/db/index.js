8cc9fee4b026c9027b0a16e2c0163ed340847c73
Salvador <frrslo@yahoo.com>
2023-02-06 12:25:41 -0500

    push landing page

--- server/db/index.js
+++ server/db/index.js
@@ -11,14 +11,14 @@ const Recipe = require("./models/Recipe");
 
 module.exports = {
   db,
-<<<<<<< HEAD
+
   User,
   
-=======
+
   Cooked,
   Ingredient,
   Pantry,
   Recipe,
-  User
->>>>>>> origin
+  User,
+
 };
