938f3f21d61ab7c0fd0b3682c727521f7018878c
Salvador <frrslo@yahoo.com>
2023-02-07 11:08:59 -0500

    cleaned up filter and took away the "tag"

--- client/components/filters.js
+++ client/components/filters.js
@@ -1,41 +1,32 @@
 import React, { useState } from 'react';
-import { connect } from 'react-redux';
 
-const Filter = ({}) => {
-  const [tagInput, setTagInput] = useState('');
-const [tags, setTags] = useState([]);
+const Filter = () => {
+  const [missingIngredients, setMissingIngredients] = useState(0);
+  const [maxCostPerServing, setMaxCostPerServing] = useState(0);
+  const [dietaryRestrictions, setDietaryRestrictions] = useState({
+    dairyFree: false,
+    soyFree: false,
+    glutenFree: false,
+    vegan: false,
+    vegetarian: false,
+    paleo: false,
+    whole30: false,
+    ketogenic: false,
+  });
 
-  const handleTagInputChange = (e) => {
-    setTagInput(e.target.value);
-  };
-
-  const handleAddTag = (e) => {
-    e.preventDefault();
-    setTagInput('');
+  const handlemaxCostPerServing = (event) => {
+    setMaxCostPerServing(event.target.value);
   };
 
   const handleFilterChange = (filter) => {
+    setDietaryRestrictions({ ...dietaryRestrictions, [filter]: !dietaryRestrictions[filter] });
   };
 
-  const handleRemoveTag = (tagToRemove) => {
-   
+  const handleSliderChange = (event) => {
+    setMissingIngredients(event.target.value);
   };
-
   return (
     <div>
-      <form onSubmit={handleAddTag}>
-        <input type="text" value={tagInput} onChange={handleTagInputChange} />
-        <button type="submit">Add Tag</button>
-      </form>
-      <ul>
-        {tags.map((tag) => (
-          <li key={tag}>
-            {tag}
-            <button onClick={() => handleRemoveTag(tag)}>Remove</button>
-          </li>
-        ))}
-      </ul>
-      <button>Clear All</button>
       <div>
         <label>
           <input type="checkbox" onChange={() => handleFilterChange('dairyFree')} />
@@ -46,31 +37,91 @@ const [tags, setTags] = useState([]);
           Soy-Free
         </label>
         <label>
-            <input type="checkbox" onChange={() => handleFilterChange('glutenFree')} />
-            Gluten-Free
+          <input type="checkbox" onChange={() => handleFilterChange('glutenFree')} />
+          Gluten-Free
         </label>
         <label>
-            <input type="checkbox" onChange={() => handleFilterChange('vegan')} />
-            Vegan
+          <input type="checkbox" onChange={() => handleFilterChange('vegan')} />
+          Vegan
         </label>
         <label>
-            <input type="checkbox" onChange={() => handleFilterChange('vegetarian')} />
-            Vegetarian
+          <input type="checkbox" onChange={() => handleFilterChange('vegetarian')} />
+          Vegetarian
         </label>
-        
+        <label>
+          <input type="checkbox" onChange={() => handleFilterChange('paleo')} />
+          Paleo
+        </label>
+        <label>
+          <input type="checkbox" onChange={() => handleFilterChange('whole30')} />
+          Whole30
+        </label>
+        <label>
+          <input type="checkbox" onChange={() => handleFilterChange('ketogenic')} />
+          Ketogenic
+        </label>
+        <label>
+          <input type="checkbox" onChange={() => handleFilterChange('lowFodmap')} />
+          Low Fodmap
+        </label>
+        <label> 
+          <input type="checkbox" onChange={() => handleFilterChange('lowCarb')} />
+          Low Carb
+        </label>
+        <label>
+          <input type="checkbox" onChange={() => handleFilterChange('lowFat')} />
+          Low Fat
+        </label>
+        <label>
+          <input type="checkbox" onChange={() => handleFilterChange('lowSugar')} />
+          low Sugar
+        </label>
+        <label>
+          <input type="checkbox" onChange={() => handleFilterChange('lowSodium')} />
+          Low Sodium
+        </label>
+        <label>
+          <input type="checkbox" onChange={() => handleFilterChange('HighProtein')} />
+          High Protein
+        </label>
+        <label>
+          <input type="checkbox" onChange={() => handleFilterChange('HighFiber')} />
+          High Fiber
+        </label>
+        <label>
+          <input type="checkbox" onChange={() => handleFilterChange('HighIron')} />
+          High Iron
+        </label>
+        <label> 
+          <input type="checkbox" onChange={() => handleFilterChange('HighCalcium')} />
+          High Calcium
+        </label>
+
+      </div>
+      <div>
+        <label htmlFor="slider">
+          Missing Ingredients
+          <input 
+            type="range" 
+            id="slider" 
+            min="0" 
+            max="5" 
+            value={missingIngredients} 
+            onChange={handleSliderChange}
+          />
+        </label>
+        <label>
+          Max Cost Per Serving
+          <input
+            type="number"
+            value={maxCostPerServing}
+            onChange={handlemaxCostPerServing}
+          />
+        </label>
+        <p>{missingIngredients}</p>
       </div>
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
-  tags: state.tags,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  addTag: (tag) => dispatch(addTag(tag)),
-  removeTag: (tag) => dispatch(removeTag(tag)),
-  clearTags: () => dispatch(clearTags()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Filter);
+export default Filter;
