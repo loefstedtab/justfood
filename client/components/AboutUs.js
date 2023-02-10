38da24788ad2b4510afcf70caffefdaea655c39d
Armand Arslanian <aarslanian56@gmail.com>
2023-02-10 15:32:04 -0500

    Steph AboutUs and css done

--- client/components/AboutUs.js
+++ client/components/AboutUs.js
@@ -1,63 +1,80 @@
-import { all } from "axios";
-import React, { useState } from "react";
+import { all } from'axios';
+import React, { useState } from 'react';
 
 const AboutUs = () => {
   const [activeIndex, setActiveIndex] = useState(-1);
-
   const handleClick = index => {
     setActiveIndex(index === activeIndex ? -1 : index);
   };
-
+  
   return (
-    <div>
-      <h2>About Just Food: </h2>
-      <p>Lorem ipsum dolor sit </p>
-      <h2>Meet the Team: </h2>
-      <div className="accordion">
-        <div className="accordion-item">
-          <button className="accordion-header" onClick={() => handleClick(0)}>
-            Armand Arslanian
-          </button>
-          <div className={`accordionContent ${activeIndex === 0 ? 'active' : ''}`}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <button className="accordion-header" onClick={() => handleClick(1)}>
-            Arthur Loefstedt
-          </button>
-          <div className={`accordionContent ${activeIndex === 1 ? 'active' : ''}`}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <button className="accordion-header" onClick={() => handleClick(1)}>
-            Lionel Verrier
-          </button>
-          <div className={`accordionContent ${activeIndex === 1 ? 'active' : ''}`}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <button className="accordion-header" onClick={() => handleClick(1)}>
-            Stephanie Kwong
-          </button>
-          <div className={`accordionContent ${activeIndex === 1 ? 'active' : ''}`}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <button className="accordion-header" onClick={() => handleClick(2)}>
-            Salvador Fierro
-          </button>
-          <div className={`accordionContent ${activeIndex === 2 ? 'active' : ''}`}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-            </div>
-            </div>
-            </div>
-            );
-          };
-
-
+    <div className='center'>
+      <section id='item1'>
+        <a href='#item1' onClick={() => handleClick(0)}>
+          Armand Arslanian
+        </a>
+        {activeIndex === 0 && (
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt vel
+            beatae consequatur suscipit vero magnam nulla molestias alias
+            praesentium atque, commodi error quis, unde aliquam distinctio
+            blanditiis! Beatae, temporibus reprehenderit!
+          </p>
+        )}
+      </section>
+      <section id='item2'>
+        <a href='#item2' onClick={() => handleClick(1)}>
+          Arthur Loefstedt
+        </a>
+        {activeIndex === 1 && (
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt vel
+            beatae consequatur suscipit vero magnam nulla molestias alias
+            praesentium atque, commodi error quis, unde aliquam distinctio
+            blanditiis! Beatae, temporibus reprehenderit!
+          </p>
+        )}
+      </section>
+      <section id='item3'>
+        <a href='#item3' onClick={() => handleClick(2)}>
+          Stephanie Kwong
+        </a>
+        {activeIndex === 2 && (
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt vel
+            beatae consequatur suscipit vero magnam nulla molestias alias
+            praesentium atque, commodi error quis, unde aliquam distinctio
+            blanditiis! Beatae, temporibus reprehenderit!
+          </p>
+        )}
+      </section>
+      <section id='item4'>
+        <a href='#item4' onClick={() => handleClick(3)}>
+          Salvador Fierro
+        </a>
+        {activeIndex ===3 && (
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt vel
+            beatae consequatur suscipit vero magnam nulla molestias alias
+            praesentium atque, commodi error quis, unde aliquam distinctio
+            blanditiis! Beatae, temporibus reprehenderit!
+          </p>
+        )}
+      </section>
+      <section id='item5'>
+        <a href='#item5' onClick={() => handleClick(4)}>
+          Lionel Verrier
+        </a>
+        {activeIndex === 4 && (
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt vel
+            beatae consequatur suscipit vero magnam nulla molestias alias
+            praesentium atque, commodi error quis, unde aliquam distinctio
+            blanditiis! Beatae, temporibus reprehenderit!
+          </p>
+        )}
+      </section>
+    </div>
+  );
+};
 export default AboutUs;
