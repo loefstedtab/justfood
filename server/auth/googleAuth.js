ad3e15226f37b8a6c4f670de26a917de74809bab
Arthur Loefstedt <theloefstedtab@gmail.com>
2023-02-12 15:28:31 -0500

    cleaned up googleAuth

--- server/auth/googleAuth.js
+++ server/auth/googleAuth.js
@@ -14,29 +14,22 @@ passport.use(
     },
     async (_, __, profile, done) => {
       let user = profile._json;
-      console.log("this is the user from the google strategy", user);
       try {
-        console.log("does the user get inside the try? ", user)
         const userExists = await User.findOne({
           where: { googleId: user.sub },
         });
         console.log(userExists)
         let findOrCreate = async () => {
           if (userExists === null) {
-            console.log("this is the user inside teh if", user)
             let newUser = await User.create({firstName: user.given_name, lastName: user.family_name, email: user.email, googleId: user.sub});
-            console.log("this is the nuw user", newUser.dataValues)
             return newUser
           } else {
-            console.log("this is the user inside teh else", user)
             let user = await userExists.update({firstName: user.given_name, lastName: user.family_name, email: user.email});
-            console.log("this is the user", user)
             return user
           }
         }
         let googleUser = await findOrCreate()
         let {dataValues} = await User.findOne({where : {googleId: googleUser.dataValues.googleId}, include: Recipe})
-        console.log("this is the google user from the findorcreate function", dataValues)
         done(null, dataValues);
       } catch (error) {
         console.log(error);
